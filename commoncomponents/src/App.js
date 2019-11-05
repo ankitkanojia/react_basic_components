f77fea46fab99a7aabc7852565f0dab303d9119e
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-05 16:56:35 +0530

    bootstrap package added
    bootstrap package added and navbar html design is settled in header file

--- commoncomponents/src/App.js
+++ commoncomponents/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Header from './Layout/Header';
 import Main from './Layout/Main';
 
--- commoncomponents/src/Layout/Header.js
+++ commoncomponents/src/Layout/Header.js
@@ -7,10 +7,27 @@ class Header extends Component {
         return (
             <React.Fragment>
                 <header>
-                    <ul>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <a className="navbar-brand" href="#">COMPONENTS</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                                <li className={"nav-item " + (this.props.location.pathname === "/" ? " active" : "")}>
+                                    <Link className="nav-link" to="/">Home</Link> <span className="sr-only">(current)</span>
+                                </li>
+                                <li className={"nav-item" + (this.props.location.pathname === "/imageverification" ? " active" : "")}>
+                                    <Link className="nav-link" to="/imageverification">Image Verification</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    {/* <ul>
                         <li className={this.props.location.pathname === "/" ? "active" : ""}><Link to="/">Home</Link></li>
                         <li className={this.props.location.pathname === "/imageverification" ? "active" : ""}><Link to="/imageverification">Image Verification</Link></li>
-                    </ul>
+                    </ul> */}
                 </header>
             </React.Fragment>
         )
