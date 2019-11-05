2db263d034ac7aed1f0b91fafa6d516887fb375d
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-05 17:09:37 +0530

    wrong image name integrate
    wrong image name integrate so that component will error handled and display default image in case of image is not available in folder strucure.

--- commoncomponents/src/Pages/ImageVerification.js
+++ commoncomponents/src/Pages/ImageVerification.js
@@ -5,9 +5,10 @@ class Home extends Component {
     render() {
         return (
             <React.Fragment>
-                <div className="row">
+                <div className="row p-3">
                     <div className="col-md-12">
                       <CustomImage width="200" src="logo.svg" className="img-thumbnail" alt="logo image" />
+                      <CustomImage width="200" src="wrong_image.svg" className="img-thumbnail ml-2" alt="logo image" />
                     </div>
                 </div>
             </React.Fragment>
