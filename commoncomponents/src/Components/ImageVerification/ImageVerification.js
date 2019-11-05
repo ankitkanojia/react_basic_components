2098c31f66732ec0f9f8249a0367aff7734bd08c
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-05 17:02:44 +0530

    Image varification component integrated
    Image verification component integrated and sample logo image file is displayed using image verification component.

--- commoncomponents/src/Components/ImageVerification/ImageVerification.js
+++ commoncomponents/src/Components/ImageVerification/ImageVerification.js
@@ -4,9 +4,9 @@ class ImageVerification extends Component {
     render() {
         let image_path = '';
         try {
-            image_path = require('./images/' + this.props.src);
+            image_path = require('./../../images/' + this.props.src);
         } catch{
-            image_path = require('./images/no_image.svg');
+            image_path = require('./../../images/no_image.svg');
         }
         return (
             <img width={this.props.width} src={image_path} className={this.props.className} alt={this.props.alt} />
--- commoncomponents/src/Pages/ImageVerification.js
+++ commoncomponents/src/Pages/ImageVerification.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import CustomImage  from "./../Components/ImageVerification/ImageVerification";
 
 class Home extends Component {
     render() {
         return (
             <React.Fragment>
-                <div>Image Verification</div>
+                <div className="row">
+                    <div className="col-md-12">
+                      <CustomImage width="200" src="logo.svg" className="img-thumbnail" alt="logo image" />
+                    </div>
+                </div>
             </React.Fragment>
         )
     }
