20eac4ac4f655626f5b0c2d0c3b2007cd3ffb39b
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-07 20:44:14 +0530

    new package for paginaion installed

--- react_basic_components/src/Pages/Pagination.js
+++ react_basic_components/src/Pages/Pagination.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import PaginationComponent from "react-js-pagination";
+import 'bootstrap-less/bootstrap/bootstrap.less';
 
 class Pagination extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            activePage: 15
+        };
+    }
+
+    handlePageChange(pageNumber) {
+        console.log(`active page is ${pageNumber}`);
+        this.setState({ activePage: pageNumber });
+    }
+
     render() {
         return (
             <React.Fragment>
-                <div>Pagination Page</div>
+                <div>
+                    <PaginationComponent
+                        activePage={this.state.activePage}
+                        itemsCountPerPage={10}
+                        totalItemsCount={450}
+                        pageRangeDisplayed={5}
+                        onChange={this.handlePageChange.bind(this)}
+                    />
+                </div>
             </React.Fragment>
         )
     }
