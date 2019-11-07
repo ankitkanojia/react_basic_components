f8740c8646207b83b9999599bbca51bca016ccee
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-07 20:21:35 +0530

    fusion package integration
    sample line chart integration added in chart.js file

--- react_basic_components/src/Pages/Chart.js
+++ react_basic_components/src/Pages/Chart.js
@@ -1,12 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import ReactFC from 'react-fusioncharts';
+ 
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+ 
+const dataSource = {
+  chart: {
+    caption: 'Countries With Most Oil Reserves [2017-18]',
+    subCaption: 'In MMbbl = One Million barrels',
+    xAxisName: 'Country',
+    yAxisName: 'Reserves (MMbbl)',
+    numberSuffix: 'K',
+    theme: 'fusion'
+  },
+  data: [
+    { label: 'Venezuela', value: '290' },
+    { label: 'Saudi', value: '260' },
+    { label: 'Canada', value: '180' },
+    { label: 'Iran', value: '140' },
+    { label: 'Russia', value: '115' },
+    { label: 'UAE', value: '100' },
+    { label: 'US', value: '30' },
+    { label: 'China', value: '30' }
+  ]
+};
+ 
+const chartConfigs = {
+  type: 'column2d',
+  width: 600,
+  height: 400,
+  dataFormat: 'json',
+  dataSource: dataSource
+};
 
-class Chart extends Component {
+class Chart extends React.Component {
     render() {
         return (
-            <React.Fragment>
-                <div>Chart Page</div>
-            </React.Fragment>
-        )
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+                      <ReactFC {...chartConfigs} />
+                    </div>
+                </div>
+            </div>)
     }
 }
 
