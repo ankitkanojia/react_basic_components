a7a636376250c7c77e37118edea723bd4c291bbf
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-09 11:25:36 +0530

    Tree structure integtrated
    Tree structure to set interlinks for all components is developed on home page

--- react_basic_components/src/Pages/Home.js
+++ react_basic_components/src/Pages/Home.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import "./../Components/TreeView/TreeView.css"
 
 class Home extends Component {
     render() {
         return (
             <React.Fragment>
-                <div>Home Page</div>
-            </React.Fragment>
-        )
-    }
-}
+                <br />
+                <div class="row">
+                    <div class="col-md-3">
+                        <div>
+                            <h3 class="text-danger">API Repository</h3>
+                            <ul class="tree">
+                                <li>
+                                    Login API
+                    <ul>
+                                        <li><a href="true">Link</a></li>
+                                        <li><a href="true">Link</a></li>
+                                        <li><a href="true">Link</a></li>
+                                        <li><a href="true">Link</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    </div>
 
+            </React.Fragment>
+                )
+            }
+        }
+        
 export default Home;
