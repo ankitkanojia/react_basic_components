d50fcc70b901e6b0e3bfd5ce27777bcace5cc367
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-10 19:16:26 +0530

    Syntax highlighter integrated in scrollbar component

--- react_basic_components/src/Pages/Scrollbar.js
+++ react_basic_components/src/Pages/Scrollbar.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 import CustomScrollbars from "./../Components/CustomScrollbars/CustomScrollbars";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { sunburst } from "react-syntax-highlighter/dist/styles";
 
 class Scrollbar extends Component {
     render() {
+        const defaultSnippet = `render() {
+    return (
+                <ul style={{ height: 500 }}>
+                    <CustomScrollbars>
+                        {[...Array(50)].map((data, index) => {
+                            return <li>Link {index}</li>
+                        })}
+                    </CustomScrollbars>
+                </ul>
+            );
+}`;
+
         return (
             <React.Fragment>
                 <div className="row mt-3">
-                    <div className="col-md-12">
+                    <div className="col-md-6">
                         <ul style={{ height: 500 }}>
-                        <CustomScrollbars>
+                            <CustomScrollbars>
                                 {[...Array(50)].map((data, index) => {
                                     return <li>Link {index}</li>
                                 })}
-                        </CustomScrollbars>
+                            </CustomScrollbars>
                         </ul>
                     </div>
+                    <div className="col-md-6">
+                        <div className='app'>
+                            <div className="panel panel-default">
+                                <div className="panel-heading">
+                                    <a href='#default'>
+                                        <h4 id='default' className="panel-title">Default</h4>
+                                    </a>
+                                </div>
+                                <div className="panel-body">
+                                    <SyntaxHighlighter language='javascript' style={sunburst}>{defaultSnippet}</SyntaxHighlighter>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
             </React.Fragment>
         )
     }
