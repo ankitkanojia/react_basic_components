e6de981152f40b9e68f85fc064a552a9041a1f75
Ankit Kanojia <ankit.kanojia@devdigital.com>
2020-09-22 11:37:25 +0530

    Simple text chat design added

---
+++ src/Pages/TextChat.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './TextChat.css';
+
+class TextChat extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div id='bodybox'>
+                    <div id='chatborder'>
+                        <p id="chatlog7" class="chatlog">&nbsp;</p>
+                        <p id="chatlog6" class="chatlog">&nbsp;</p>
+                        <p id="chatlog5" class="chatlog">&nbsp;</p>
+                        <p id="chatlog4" class="chatlog">&nbsp;</p>
+                        <p id="chatlog3" class="chatlog">&nbsp;</p>
+                        <p id="chatlog2" class="chatlog">&nbsp;</p>
+                        <p id="chatlog1" class="chatlog">&nbsp;</p>
+                        <input type="text" name="chat" id="chatbox" placeholder="Hi there! Type here to talk to me." onfocus="placeHolder()" />
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default TextChat;
