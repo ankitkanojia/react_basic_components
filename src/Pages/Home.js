374e16ad16b8fe6fd6f5d72fc0f8791eb0b0fa31
Ankit Kanojia <ankit.kanojia@devdigital.com>
2020-10-07 16:18:58 +0530

    Added notification link in list of links

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,31 +9,38 @@ class Home extends Component {
                 <br />
                 <div className="row">
                     <div className="col-md-3">
-                        <Link to="/treeview"><button style={{height : "100px", minWidth: "100px"}} className="btn btn-success">TreeView</button></Link>
+                        <Link to="/treeview"><button style={{ height: "100px", minWidth: "100px" }} className="btn btn-success">TreeView</button></Link>
                     </div>
                     <div className="col-md-3">
-                        <Link to="/imageverification"><button style={{height : "100px", minWidth: "100px"}}  className="btn btn-success">Image Verification</button></Link>
+                        <Link to="/imageverification"><button style={{ height: "100px", minWidth: "100px" }} className="btn btn-success">Image Verification</button></Link>
                     </div>
                     <div className="col-md-3">
-                        <Link to="/scrollbar"><button style={{height : "100px", minWidth: "100px"}}  className="btn btn-success">ScrollBar</button></Link>
+                        <Link to="/scrollbar"><button style={{ height: "100px", minWidth: "100px" }} className="btn btn-success">ScrollBar</button></Link>
                     </div>
                     <div className="col-md-3">
-                        <Link to="/charts"><button style={{height : "100px", minWidth: "100px"}}  className="btn btn-success">Charts</button></Link>
+                        <Link to="/charts"><button style={{ height: "100px", minWidth: "100px" }} className="btn btn-success">Charts</button></Link>
                     </div>
                 </div>
                 <br />
                 <div className="row">
                     <div className="col-md-3">
-                        <Link to="/pagination"><button style={{height : "100px", minWidth: "100px"}} className="btn btn-success">Pagiation</button></Link>
+                        <Link to="/pagination"><button style={{ height: "100px", minWidth: "100px" }} className="btn btn-success">Pagiation</button></Link>
                     </div>
                     <div className="col-md-3">
-                        <Link to="/tablepagination"><button style={{height : "100px", minWidth: "100px"}} className="btn btn-success">TablePagination</button></Link>
+                        <Link to="/tablepagination"><button style={{ height: "100px", minWidth: "100px" }} className="btn btn-success">TablePagination</button></Link>
                     </div>
                     <div className="col-md-3">
-                        <Link to="/dragdrop"><button style={{height : "100px", minWidth: "100px"}} className="btn btn-success">Drag-Drop</button></Link>
+                        <Link to="/dragdrop"><button style={{ height: "100px", minWidth: "100px" }} className="btn btn-success">Drag-Drop</button></Link>
                     </div>
                     <div className="col-md-3">
-                        <Link to="/textchat"><button style={{height : "100px", minWidth: "100px"}} className="btn btn-success">Text-Chat</button></Link>
+                        <Link to="/textchat"><button style={{ height: "100px", minWidth: "100px" }} className="btn btn-success">Text-Chat</button></Link>
+                    </div>
+
+                </div>
+                <br />
+                <div className="row">
+                    <div className="col-md-3">
+                        <Link to="/notification"><button style={{ height: "100px", minWidth: "100px" }} className="btn btn-success">Push-Notification</button></Link>
                     </div>
                 </div>
 
