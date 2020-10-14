80f7672122b6605f88af53528fb23526336f9b28
Ankit Kanojia <ankit.kanojia@devdigital.com>
2020-10-14 14:08:45 +0530

    Integrate Get method For firebase API

--- src/Pages/FirebaseAPIs.js
+++ src/Pages/FirebaseAPIs.js
@@ -33,51 +33,6 @@ class FirebaseAPIs extends Component {
       });
   }
 
-  addDetails = (Obj) => {
-    // Add document using delete option from firestore database
-    FireBaseContext().collection("Students")
-      .doc(obj.Id)
-      .set(obj)
-      .then(function () {
-        // Student details addedd in firebase database  
-        console.log('Student details successfully added!');
-      })
-      .catch(function (error) {
-        // Error or Exception occure
-        console.error("Error writing document: ", error);
-      });
-  }
-
-  updateDetails = (Obj) => {
-    // Add document using delete option from firestore database
-    FireBaseContext().collection("Students")
-      .doc()
-      .set(obj)
-      .then(function () {
-        // Student details addedd in firebase database  
-        console.log('Student details successfully added!');
-      })
-      .catch(function (error) {
-        // Error or Exception occure
-        console.error("Error writing document: ", error);
-      });
-  }
-
-  deleteDetails = (Obj) => {
-    // Delete document using delete option from firestore database
-    FireBaseContext().collection("Students")
-      .doc(Obj.Id)
-      .delete()
-      .then(function () {
-        // Student details deleted from firebase database  
-        console.log("Student details successfully deleted!");
-      }).catch(
-        function (error) {
-          // Error or Exception occure
-          console.error("Error removing document: ", error);
-        });
-  }
-
   render() {
     return (
       <React.Fragment>
