0cd5aa15b49350caea47141de2022d77a5fbc9d1
Ankit Kanojia <ankit.kanojia@devdigital.com>
2020-10-19 14:55:00 +0530

    Integrate syntax highlighter plugin to highlight specific code

--- src/Pages/ImageVerification.js
+++ src/Pages/ImageVerification.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import CustomImage  from "./../Components/ImageVerification/ImageVerification";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { sunburst } from "react-syntax-highlighter/dist/styles";
 
 class Home extends Component {
     render() {
@@ -9,6 +11,20 @@ class Home extends Component {
                     <div className="col-md-6">
                       <CustomImage width="200" src="logo.svg" className="img-thumbnail" alt="logo image" />
                       <CustomImage width="200" src="wrong_image.svg" className="img-thumbnail ml-2" alt="logo image" />
+                    </div>
+                    <div className="col-md-6">
+                        <div className='app'>
+                            <div className="panel panel-default">
+                                <div className="panel-heading">
+                                    <a href='#default'>
+                                        <h4 id='default' className="panel-title">Default</h4>
+                                    </a>
+                                </div>
+                                <div className="panel-body">
+                                    <SyntaxHighlighter language='javascript' style={sunburst}></SyntaxHighlighter>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </React.Fragment>
