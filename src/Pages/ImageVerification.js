984416893ba98c3f92649da4d6124b2e504c29bf
Ankit Kanojia <ankit.kanojia@devdigital.com>
2020-10-27 09:57:24 +0530

    Handling Image verification functinality

--- src/Pages/ImageVerification.js
+++ src/Pages/ImageVerification.js
@@ -5,26 +5,6 @@ import { sunburst } from "react-syntax-highlighter/dist/styles";
 
 class Home extends Component {
     render() {
-
-        const defaultSnippet = `import React, { Component } from 'react';
-
-class ImageVerification extends Component {
-    render() {
-        let image_path = '';
-        try {
-                image_path = require('./../../images/' + this.props.src);
-            } catch{
-                image_path = require('./../../images/no_image.svg');
-            }
-   
-            return (
-                    <img width={this.props.width} src={image_path} className={this.props.className} alt={this.props.alt} />
-            );
-        }
-    }
-        
-export default ImageVerification;`;
-
         return (
             <React.Fragment>
                 <div className="row p-3">
