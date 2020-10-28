0db9a46d3346d7738a69bf4c1464c054d62b431f
Ankit Kanojia <ankit.kanojia@devdigital.com>
2020-10-28 17:13:40 +0530

    Upgrade the configuration with chart implementation.

--- src/Pages/Chart.js
+++ src/Pages/Chart.js
@@ -1,4 +1,39 @@
 import React from 'react';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import ReactFC from 'react-fusioncharts';
+ 
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+ 
+const dataSource = {
+  chart: {
+    caption: 'Countries With Most Oil Reserves [2017-18]',
+    subCaption: 'In MMbbl = One Million barrels',
+    xAxisName: 'Country',
+    yAxisName: 'Reserves (MMbbl)',
+    numberSuffix: 'K',
+    theme: 'fusion'
+  },
+  data: [
+    { label: 'Venezuela', value: '290' },
+    { label: 'Saudi', value: '260' },
+    { label: 'Canada', value: '180' },
+    { label: 'Iran', value: '140' },
+    { label: 'Russia', value: '115' },
+    { label: 'UAE', value: '100' },
+    { label: 'US', value: '30' },
+    { label: 'China', value: '30' }
+  ]
+};
+ 
+const chartConfigs = {
+  type: 'column2d',
+  width: 600,
+  height: 400,
+  dataFormat: 'json',
+  dataSource: dataSource
+};
 
 class Chart extends React.Component {
     render() {
@@ -6,7 +41,7 @@ class Chart extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="col-sm-6">
-                   
+                      <ReactFC {...chartConfigs} />
                     </div>
                 </div>
             </div>)
