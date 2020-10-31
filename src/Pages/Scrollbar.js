2a8f7fc84bed01da8bdef63113500b93fed02366
Ankit Kanojia <ankit.kanojia@devdigital.com>
2020-10-31 18:13:38 +0530

    Remove scrollbar snippet as need to upgrade the functionality.

--- src/Pages/Scrollbar.js
+++ src/Pages/Scrollbar.js
@@ -5,18 +5,6 @@ import { sunburst } from "react-syntax-highlighter/dist/styles";
 
 class Scrollbar extends Component {
     render() {
-        const defaultSnippet = `render() {
-    return (
-                <ul style={{ height: 500 }}>
-                    <CustomScrollbars>
-                        {[...Array(50)].map((data, index) => {
-                            return <li>Link {index}</li>
-                        })}
-                    </CustomScrollbars>
-                </ul>
-            );
-}`;
-
         return (
             <React.Fragment>
                 <div className="row mt-3">
